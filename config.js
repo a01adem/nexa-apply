18f1ff7479804bc7cd65df0f8d522d8d6c864773
A01 Dev <126718579+a01adem@users.noreply.github.com>
2024-10-23 15:51:12 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  token: 'MTI5ODYyMTU1NTQ3OTQxNjkwMg.G-tnFv.5Bz-ZG3fEfDJhoBzMiWGRT4Pd85o-76LX5H1bk' || process.env.token,
+  admins: ['785427631876472892', "915689279605342218"],
+  clientId: '1298595629177638932',
+  clientSecret: 'f9tnxySbwJfeKU8ELjBmU3tt75sY02WB',
+  callbackUrl: 'https://b1b5ae78-c1eb-4a6b-be4b-f7ea8d2db0b8-00-10yokkp5rixw1.worf.replit.dev/auth/discord/callback',
+  mongoDb: 'mongodb+srv://dislist:dislist01@cluster0.2v3bdjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  guildId: '1001820047091699852',
+  roles: {
+    supportId: '1209189831108722689',
+  },
+  
